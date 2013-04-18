6862cc8787fadcb2c6eaafafd28795d8368cdefb
Sushil Kundu <sushilkundu@Beta-Romeo.local>
2013-04-18 12:55:25 +0530

    Initial Commit

---
+++ EtSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EtSample
+//
+//  Created by Sushil Kundu on 18/04/13.
+//  Copyright (c) 2013 Sushil Kundu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EtSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EtSample
+//
+//  Created by Sushil Kundu on 18/04/13.
+//  Copyright (c) 2013 Sushil Kundu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
